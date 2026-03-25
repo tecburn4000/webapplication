6dd5abf4cdad27fefb9db221124d047b3f9cde76
poppenberg <j.poppenberg@ccv.eu>
2026-03-25 08:53:01 +0100

    added rest permission annotation

---
+++ src/main/java/com/example/webapplication/security/permissions/PermissionRestApi.java
@@ -0,0 +1,11 @@
+package com.example.webapplication.security.permissions;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER') or @securityProperties.permitsAll")
+public @interface PermissionRestApi {
+}
