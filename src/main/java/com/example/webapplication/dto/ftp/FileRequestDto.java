9ae3aa824a86417238316b4b7725eb1ed56dc530
poppenberg <j.poppenberg@ccv.eu>
2026-03-25 08:52:06 +0100

    lombok stuff

--- src/main/java/com/example/webapplication/dto/ftp/FileRequestDto.java
+++ src/main/java/com/example/webapplication/dto/ftp/FileRequestDto.java
@@ -7,7 +7,4 @@ import lombok.Data;
 @Builder
 public class FileRequestDto {
     private String filePath;
-
-    public String getFilePath() { return filePath; }
-    public void setFilePath(String filePath) { this.filePath = filePath; }
 }
